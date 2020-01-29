d8dcd9a8adcd87242fdbe7852834e3009319485d
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-29 12:25:16 -0400

    Adds validations for User email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   validates :username, presence: true,
-                       length: { minimum: 4, maximum: 50 },
-                       uniqueness: true
+                      length: { minimum: 4, maximum: 50 },
+                      uniqueness: true
+  validates :email,  presence: true,
+    length: { minimum: 4, maximum: 50 },
+    uniqueness: true
+  validates :password,   presence: true,
+  length: { minimum: 4, maximum: 50 }
 end

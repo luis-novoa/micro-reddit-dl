cbcd121ca3847f76e06dec6c6eac11d6a296cdbf
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-29 13:04:44 -0400

    Creates model and validations for Comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
             length: {minimum: 4, maximum: 75}
   validates :link, presence: true,
             length: {minimum: 4, maximum: 250}
+  validates :user_id, presence: true
   belongs_to :user
 end

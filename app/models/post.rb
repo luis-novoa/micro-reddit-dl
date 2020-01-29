71b3542ae5bea0ac67865f337576f401e54a789f
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-29 12:27:35 -0400

    Creates Post model and validations for it

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :title, presence: true,
+    length: { minimum: 4, maximum: 75 }
+  validates :link, presence: true,
+  length: { minimum: 4, maximum: 250 }
+end

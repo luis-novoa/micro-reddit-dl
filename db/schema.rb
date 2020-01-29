6d4859f314a5142a92e05f21fc31aa303340a8af
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-29 12:29:59 -0400

    Migrated database (Posts)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_155142) do
+ActiveRecord::Schema.define(version: 2020_01_29_162248) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
